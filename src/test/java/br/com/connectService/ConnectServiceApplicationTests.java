6e7da95533e5a6ea853e114f383f2b7a052acbef
Aogawa <aogawa@fatecie.edu.br>
2026-03-20 13:26:33 -0300

    feat: push project to gitHub

---
+++ src/test/java/br/com/connectService/ConnectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.connectService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
